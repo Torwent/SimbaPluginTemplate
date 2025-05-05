3a20cc68da913bed9c8e4e45ec6975b5240c4103
Torwent <Torwent@users.noreply.github.com>
2025-05-05 13:47:30 +0200

    template finished

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -24,7 +24,7 @@ const PASCAL_EXPORTS: &[(&str, &str)] = &[
 // dll functions
 #[no_mangle]
 pub extern "C" fn TestHelloWorld() {
-    println!("Hello World!");
+    println!("Hello World!\r\n");
 }
 
 #[no_mangle]
